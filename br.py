c3d1184bb2eec62954361c34b45a614a471a1e9d
Henry Teigar <henry.teigar@gmail.com>
2018-01-09 13:24:43 +0200

    Add new files

--- br.py
+++ br.py
@@ -1,4 +1,4 @@
-""" Trains an agent with (stochastic) Policy Gradients on Pong. Uses OpenAI Gym. """
+
 import numpy as np
 import _pickle as pickle
 import gym
@@ -7,13 +7,13 @@ import time
 import keras
 
 
-# hyperparameters
-H = 200  # number of hidden layer neurons
-batch_size = 10  # every how many episodes to do a param update?
+
+H = 200
+batch_size = 10
 learning_rate = 1e-4
-decay_rate = 0.99  # decay factor for RMSProp leaky sum of grad^2
-gamma = 0.99  # discount factor for reward
-resume = True  # resume from previous checkpoint?
+decay_rate = 0.99
+gamma = 0.99
+resume = True
 render = False
 
 # model initialization
@@ -43,7 +43,6 @@ def prepro(I):
 
 
 def discount_rewards(r):
-    """ take 1D float array of rewards and compute discounted reward """
     discounted_r = np.zeros_like(r)
     running_add = 0
     for t in reversed(range(0, r.size)):
@@ -62,7 +61,6 @@ def policy_forward(x):
 
 
 def policy_backward(eph, epdlogp):
-    """ backward pass. (eph is array of intermediate hidden states) """
     dW2 = np.dot(eph.T, epdlogp).ravel()
     dh = np.outer(epdlogp, model['W2'])
     dh[eph <= 0] = 0  # backpro prelu
@@ -70,7 +68,6 @@ def policy_backward(eph, epdlogp):
     return {'W1': dW1, 'W2': dW2}
 
 
-# env = gym.make("Pong-v0")
 env = gym.make("Breakout-v0")
 observation = env.reset()
 prev_x = None  # used in computing the difference frame
@@ -84,12 +81,10 @@ while True:
     env.step(1)
     if render: env.render()
 
-    # preprocess the observation, set input to network to be difference image
     cur_x = prepro(observation)
     x = cur_x - prev_x if prev_x is not None else np.zeros(D)
     prev_x = cur_x
 
-    # forward the policy network and sample an action from the returned probability
     aprob, h = policy_forward(x)
 
     action = 2 if np.random.uniform() < aprob else 3  # roll the dice!
@@ -99,7 +94,7 @@ while True:
     hs.append(h)  # hidden state
     y = 1 if action == 2 else 0  # a "fake label"
     dlogps.append(
-        y - aprob)  # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
+        y - aprob)
 
     # step the environment and get new measurements
     observation, reward, done, info = env.step(action)
@@ -131,15 +126,13 @@ while True:
         epr = np.vstack(drs)
         xs, hs, dlogps, drs = [], [], [], []  # reset array memory
 
-        # compute the discounted reward backwards through time
         discounted_epr = discount_rewards(epr)
-        # standardize the rewards to be unit normal (helps control the gradient estimator variance)
         discounted_epr -= np.mean(discounted_epr)
         discounted_epr /= np.std(discounted_epr)
 
-        epdlogp *= discounted_epr  # modulate the gradient with advantage (PG magic happens right here.)
+        epdlogp *= discounted_epr # Gradientide läbi korrutamine
         grad = policy_backward(eph, epdlogp)
-        for k in model: grad_buffer[k] += grad[k]  # accumulate grad over batch
+        for k in model: grad_buffer[k] += grad[k]
 
         # perform rmsprop parameter update every batch_size episodes
         if episode_number % batch_size == 0:
@@ -149,7 +142,6 @@ while True:
                 model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
                 grad_buffer[k] = np.zeros_like(v)  # reset batch gradient buffer
 
-        # boring book-keeping
         running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
         print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
 
