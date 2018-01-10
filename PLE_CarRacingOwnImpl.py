e34d2d62ba032cfaede842694d52d72460dbbfa9
Henry Teigar <henry.teigar@gmail.com>
2018-01-10 08:53:25 +0200

    Finish PLE implementation

--- PLE_CarRacingOwnImpl.py
+++ PLE_CarRacingOwnImpl.py
@@ -3,28 +3,36 @@ from ple import PLE
 import numpy as np
 import time
 
+
+render = False
+
+def process_state(state):
+    return np.array(state.values())
+
 game = CarRacingOwnImpl()
-p = PLE(game, fps=30)
+p = PLE(game, fps=30, state_preprocessor=process_state, display_screen=render)
 
 p.init()
 i = 0
 
-for i in range(np.random.randint(0, 20)):
-    reward = p.act(p.NOOP)
+
 
 while True:
     # if the game is over
     if p.game_over():
+        print(p.score())
         p.reset_game()
 
     obs = p.getScreenRGB()
-    action = game.actions['left']
+    #action = game.actions['left']
+    action = p.NOOP
     reward = p.act(action)
-    print(reward)
+
     i+=1
     if i % 60 == 0:
         p.saveScreen("screen_capture.png")
 
-    #time.sleep(1)
+
+
 
 
